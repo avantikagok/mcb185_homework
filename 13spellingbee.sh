7ee814fc226a000aff9e6c0ee9c3dfdd04e84574
avantikagok <agokulnatha@ucdavis.edu>
2024-01-20 12:49:44 -0800

    commit

--- 13spellingbee.sh
+++ 13spellingbee.sh
@@ -1,2 +1,3 @@
 cd ~/Code/MCB185/data
 gunzip -c dictionary.gz | grep -E "^[crazion]+$" | grep -E "r" | grep -E "...." 
+gunzip -c dictionary.gz | grep -E "^[crazion]+$" | grep -E "r" | grep -E "...." | wc -l
