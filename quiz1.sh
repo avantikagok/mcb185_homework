366fd0eea864da619180ffe4f21ae1d4fcd96f0c
avantikagok <agokulnatha@ucdavis.edu>
2024-01-23 09:21:49 -0800

    commit

--- quiz1.sh
+++ quiz1.sh
@@ -1,2 +1,19 @@
 echo "Name: Avantika Gokulnatha"
 echo "USER: $USER"
+
+cd ~/Code/MCB185/data
+gunzip -c dictionary.gz | grep -E "^[muotfca]+$" | grep -E "a" | grep -E "...." 
+gunzip -c dictionary.gz | grep -E "^[muotfca]+$" | grep -E "a" | grep -E "...."  | wc -l
+
+
+
+gunzip -c dictionary.gz | grep -E "^[tailnrb]+$" | grep -E "b" | grep -E "...." 
+gunzip -c dictionary.gz | grep -E "^[tailnrb]+$" | grep -E "b" | grep -E "...."  | wc -l
+
+
+
+gunzip -c dictionary.gz | grep -E "^[maonidc]+$" | grep -E "c" | grep -E "...." 
+gunzip -c dictionary.gz | grep -E "^[maonidc]+$" | grep -E "c" | grep -E "...."  | wc -l
+
+gunzip -c dictionary.gz | grep -E "^[anorgiz]+$" | grep -E "z" | grep -E "...." 
+gunzip -c dictionary.gz | grep -E "^[anorgiz]+$" | grep -E "z" | grep -E "...."  | wc -l
