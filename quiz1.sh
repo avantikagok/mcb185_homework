5b3da54d1993e5b472f0ecb79e8b7e3012c436bb
avantikagok <agokulnatha@ucdavis.edu>
2024-01-23 09:44:24 -0800

    commit

--- quiz1.sh
+++ quiz1.sh
@@ -7,19 +7,19 @@ gunzip -c dictionary.gz | grep -E "^[muotfca]+$" | grep -E "a" | grep -E "...." 
 
 
 
-gunzip -c dictionary.gz | grep -E "^[tailnrb]+$" | grep -E "b" | grep -E "...." 
+#gunzip -c dictionary.gz | grep -E "^[tailnrb]+$" | grep -E "b" | grep -E "...." 
 gunzip -c dictionary.gz | grep -E "^[tailnrb]+$" | grep -E "b" | grep -E "...."  | wc -l
 
 
 
-gunzip -c dictionary.gz | grep -E "^[maonidc]+$" | grep -E "c" | grep -E "...." 
+#gunzip -c dictionary.gz | grep -E "^[maonidc]+$" | grep -E "c" | grep -E "...." 
 gunzip -c dictionary.gz | grep -E "^[maonidc]+$" | grep -E "c" | grep -E "...."  | wc -l
 
-gunzip -c dictionary.gz | grep -E "^[anorgiz]+$" | grep -E "z" | grep -E "...." 
+#gunzip -c dictionary.gz | grep -E "^[anorgiz]+$" | grep -E "z" | grep -E "...." 
 gunzip -c dictionary.gz | grep -E "^[anorgiz]+$" | grep -E "z" | grep -E "...."  | wc -l
 
 
-cd ~/MCB185/data
+cd ~/Code/MCB185/data
 gunzip -c jaspar2024_core.transfac.gz | grep -E "tax" | wc -l "diatoms"
 
 
