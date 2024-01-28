76b490d273d04f4e38b9b3347fc71f4e158e6e56
avantikagok <agokulnatha@ucdavis.edu>
2024-01-27 19:01:39 -0800

    commit

--- 21quadratic.py
+++ 21quadratic.py
@@ -1,14 +1,14 @@
 import math 
 import sys
 def quadratic (a, b, c):
-	if b**2 - 4*a*c < 0:
+	if b**2 - 4 * a * c < 0:
 		return None, None, ": No real solutions"
-	elif b**2 - 4*a*c == 0:
-		x1 = (-b + (b**2 - 4*a*c)**0.5) / (2*a)
+	elif b**2 - 4 * a * c == 0:
+		x1 = (-b + (b**2 - 4 * a * c)**0.5) / (2 * a)
 		return x1, None, ": One real solution"
 	else:
-		x1 = (-b + (b**2 - 4*a*c)**0.5) / (2*a)
-		x2 = (-b - (b**2 - 4*a*c)**0.5) / (2*a)
+		x1 = (-b + (b**2 - 4 * a * c)**0.5) / (2 * a)
+		x2 = (-b - (b**2 - 4 * a * c)**0.5) / (2 * a)
 		return x1, x2, ": Two real solutions"
 
 
