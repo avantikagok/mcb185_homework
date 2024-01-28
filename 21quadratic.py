37934566ca11fdf70c1d90960f60ac018a0b647b
avantikagok <agokulnatha@ucdavis.edu>
2024-01-27 19:13:52 -0800

    commit

--- 21quadratic.py
+++ 21quadratic.py
@@ -1,6 +1,6 @@
 import math 
 import sys
-def quadratic (a, b, c):
+def quadratic(a, b, c):
 	if b**2 - 4 * a * c < 0:
 		return None, None, ": No real solutions"
 	elif b**2 - 4 * a * c == 0:
@@ -13,12 +13,12 @@ def quadratic (a, b, c):
 
 
 
-x1, x2, solnum = quadratic (5, -7, 14)
+x1, x2, solnum = quadratic(5, -7, 14)
 print(x1, x2, solnum)
 
-x1, x2, solnum = quadratic (1, -7, -3)
+x1, x2, solnum = quadratic(1, -7, -3)
 print(x1, x2, solnum)
 
-x1, x2, solnum = quadratic (1, 2, 1)
+x1, x2, solnum = quadratic(1, 2, 1)
 print(x1, x2, solnum)
 
--- 22oligotemp.py
+++ 22oligotemp.py
@@ -7,11 +7,11 @@ def tm(A, C, G, T):
 		melttemp = 64.9 + 41 * (G + C - 16.4) / (A + C + G + T)
 		return oligolen, melttemp
 		
-oligolen, melttemp = tm (10, 2, 4, 5)
+oligolen, melttemp = tm(10, 2, 4, 5)
 print("Sequence length:", str(oligolen), "Melting temp:", str(melttemp))
 
-oligolen, melttemp = tm (1, 2, 1, 5)
+oligolen, melttemp = tm(1, 2, 1, 5)
 print("Sequence length:", str(oligolen), "Melting temp:", str(melttemp))
 
-oligolen, melttemp = tm (123984, 223423, 132423, 523423)
+oligolen, melttemp = tm(123984, 223423, 132423, 523423)
 print("Sequence length:", str(oligolen), "Melting temp:", str(melttemp))
